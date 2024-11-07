af6ca4676dddb8ec5ce3924e56ca20287a634479
Arifcy7 <choudharyarif756@gmail.com>
2024-11-07 19:26:11 +0530

    convert the website to android application

---
+++ app/src/main/java/com/example/convertwebsitetoandroidapplication/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.convertwebsitetoandroidapplication
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.isDebugInspectorInfoEnabled
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.convertwebsitetoandroidapplication.ui.theme.ConvertWebsiteToAndroidApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ConvertWebsiteToAndroidApplicationTheme {
+                WebtoAndroid("https://www.youtube.com/")
+            }
+        }
+    }
+}
+
+@Composable
+fun WebtoAndroid(url: String) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
+    ) { padding ->
+        AndroidView(
+            factory = { context ->
+                WebView(context).apply {
+                    settings.javaScriptEnabled = true
+                    webViewClient = WebViewClient()
+                }
+            },
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .windowInsetsPadding(WindowInsets.systemBars),
+            update = { webView ->
+                webView.loadUrl(url)
+            }
+        )
+    }
+}
